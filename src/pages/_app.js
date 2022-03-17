1309804df6644fd4a3a5140f76f5ed37246f9a7f
tomo0714 <tomo.nyaaaaan.714@docomo.ne.jp>
2022-03-17 18:55:17 +0900

    ♻️ appでhead部分の共通化

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>DESIGN FOR SMILE</title>
+        <meta
+          name="description"
+          content="DIGSMILEは、デザインで人を豊かにする社会。なんでもない日常に少しのワクワクと遊び心を提供します。"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
